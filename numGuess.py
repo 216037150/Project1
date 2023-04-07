88a35bcab3567dd57bef929ac0ba0b6bc871cbdb
216037150 <52372746+216037150@users.noreply.github.com>
2023-04-07 15:07:47 +0200

    Add files via upload
    updated number guess program

--- numGuess.py
+++ numGuess.py
@@ -1,1 +1,19 @@
+import random 
+n = random.randrange(1,5)
+print(n)
 
+guess = int(input('Enter the number: '))
+
+while n != guess:
+    if guess<n:
+        print("The number is too low")
+        guess = int(input('Enter the number: '))
+    elif guess>n:
+        print("The number is too big")
+        guess = int(input('Enter the number: '))
+    else:
+        break        
+
+print("Congratulations, you guessed correct!!!")
+ 
+ 
